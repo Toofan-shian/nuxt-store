9028f63ef4d26bdc52d66a0fefde2f7d3211f160
toofan <toofan.shian@gmail.com>
2023-07-20 18:39:23 +0330

    project initiated some modules added

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app>
+    <v-main>
+      <nuxtPage></nuxtPage>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-h1">
+    index page
+  </div>
+  <v-icon class="text-red text-h3 ma-4 border border-green rounded ">mdi-home</v-icon>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
