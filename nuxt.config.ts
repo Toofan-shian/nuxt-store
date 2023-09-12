49c3daf3905361b091d408fa9191d9b072ec139f
toofan <toofan.shian@gmail.com>
2023-09-12 16:35:20 +0330

    adding start command and handling proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,14 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    // '/cart': {redirect: '/' },
+    routeRules: [
+      {
+        // Match requests to '/api/' and proxy them to 'http://localhost:3000'
+        // This example assumes you want to proxy all requests starting with '/api/'.
+        source: '/api/(.*)',
+        destination: 'http://localhost:3000/api/$1',
+      },
+    ],
   },
-  debug: false
+  debug: false,
 })
